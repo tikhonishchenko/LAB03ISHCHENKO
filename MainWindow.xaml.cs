f8a16b8e26523a3e33f40b43d366f0dcf8ae352e
Тіхон Іщенко <tikhon.ishchenko@ukma.edu.ua>
2023-03-20 10:23:06 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB03ISHCHENKO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void ProceedButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CalculateAge() || !Person.IsValidEmail(Email.Text))
+            {
+                return;
+            }
+            Person person = new Person(FirstName.Text, LastName.Text, Email.Text, DatePicker.SelectedDate.Value);
+            person.InitializeAsync();
+            FirstNameAns.Content = "Ім'я: "+ person.Name;
+            LastNameAns.Content = "Прізвище: " + person.Surname;
+            EmailAns.Content = "Електронна пошта: " + person.EmailAddress;
+            BirthDayAns.Content = "Дата народження: " + person.DateOfBirth.ToShortDateString();
+            IsAdult.Text = "Чи повнолітній: " + person.IsAdult;
+            HoroscopeEnglish.Text = "Знак сонця: " + person.SunSign;
+            HoroscopeAsian.Text = "Знак китайського зодіаку: " + person.ChineseSign;
+            IsBirthday.Text = "Чи є сьогодні днем народження: " + person.IsBirthday;
+
+
+        }
+        private bool CalculateAge()
+        {
+            DateTime? birthDate = DatePicker.SelectedDate;
+            if (birthDate != null)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - birthDate.Value.Year;
+                if (birthDate > today.AddYears(-age)) age--;
+                if (age > 135)
+                {
+                    throw new AgeTooLowException("Та ти точно не такий старий, давай пиши справжній");
+                }
+                else if (age < 0)
+                {
+                    throw new AgeTooHighException("Та ти точно не такий молодий, давай пиши справжній");
+                }
+                else
+                {
+                    if (birthDate.Value.Month == today.Month && birthDate.Value.Day == today.Day)
+                        MessageBox.Show("З днем народження!");
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void LastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+    }
+}
